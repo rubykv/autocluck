3f477fd5bd5d14e4f4bc16028a64207bbae8fad2
rubykv <rubyshiv@gmail.com>
2021-11-30 16:50:26 -0600

    logic to auto pick tweets updated

--- src/main/java/com/autocluck/tweet_scheduler/service/ScheduleTweetService.java
+++ src/main/java/com/autocluck/tweet_scheduler/service/ScheduleTweetService.java
@@ -1,7 +1,10 @@
 package com.autocluck.tweet_scheduler.service;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
@@ -9,12 +12,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import com.autocluck.tweet_scheduler.external_client.TwitterClient;
 import com.autocluck.tweet_scheduler.model.Identity;
 import com.autocluck.tweet_scheduler.model.Tweet;
 import com.autocluck.tweet_scheduler.repository.IdentityRepository;
 import com.autocluck.tweet_scheduler.repository.TweetRepository;
+
+import twitter4j.TwitterException;
 
 @Service
 public class ScheduleTweetService {
@@ -28,19 +34,36 @@ public class ScheduleTweetService {
 
 	@Autowired
 	private TwitterClient twitterClient;
+	
+	private List<String> publishedTweets;
 
 	@Scheduled(fixedRate = 1, timeUnit = TimeUnit.HOURS)
 	public void tweet() {
 		try {
+			//fetch tweets from DB
 			List<Tweet> tweets = tweetRepository.findAll();
-			List<Identity> identities = identityRepository.findAll();
-			Random random = new Random();
-			// TODO change logic here
-			Tweet selectedTweet = tweets.get(random.nextInt(tweets.size()));
-			String tweetToPublish = selectedTweet.getContent();
-			twitterClient.doPost(tweetToPublish, identities.get(0));
+			
+			//First time execution OR All tweets have been published at least once, empty the temp list
+			if(CollectionUtils.isEmpty(publishedTweets) || publishedTweets.size() == tweets.size()) {
+				publishedTweets = new ArrayList<>();
+			}
+			postTweetIfNotAlreadyPublished(tweets);
 		} catch (Exception ex) {
 			logger.error("Couldn't update tweet ", ex);
+		}
+	}
+
+	private void postTweetIfNotAlreadyPublished(List<Tweet> tweets) throws InterruptedException, ExecutionException, IOException, TwitterException {
+		Random random = new Random();
+		Tweet selectedTweet = tweets.get(random.nextInt(tweets.size()));
+		if(!publishedTweets.contains(selectedTweet.getName())) {
+			publishedTweets.add(selectedTweet.getName());
+			//Post tweet
+			String tweetToPublish = selectedTweet.getContent();
+			List<Identity> identities = identityRepository.findAll();
+			twitterClient.doPost(tweetToPublish, identities.get(0));
+		} else {
+			postTweetIfNotAlreadyPublished(tweets);
 		}
 	}
 }
