760a5335e79a0313083db64f23b6fafe0271e2fe
rubykv <rubyshiv@gmail.com>
2022-02-07 12:31:22 -0600

    database config updated

--- src/main/java/com/autocluck/tweet_scheduler/config/DatabaseConfig.java
+++ src/main/java/com/autocluck/tweet_scheduler/config/DatabaseConfig.java
@@ -25,7 +25,7 @@ public class DatabaseConfig extends AbstractMongoClientConfiguration {
 
 	@Override
 	public MongoClient mongoClient() {
-		final ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/test");
+		final ConnectionString connectionString = new ConnectionString("mongodb://34.201.66.129:27017/test");
 		final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
 				.applyConnectionString(connectionString).build();
 		return MongoClients.create(mongoClientSettings);
